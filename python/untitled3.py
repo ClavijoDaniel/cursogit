8513f4f9e5be28f759630ce16138283465e3d72e
danistreet <dani.street12@gmail.com>
2022-10-25 19:52:19 +0200

    primer cambios a archivos python

---
+++ python/untitled3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 19:26:41 2022
+
+@author: Daniel
+"""
+
+print("primer cambio para commit")
---
+++ python/untitled4.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 19:26:42 2022
+
+@author: Daniel
+"""
+
+#que pasa si solo pongo un comentario
